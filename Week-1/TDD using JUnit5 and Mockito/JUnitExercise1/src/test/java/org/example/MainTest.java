19bbc41c460aaf559aed1e0d642d68207aed5933
latika2006 <latikashahapurkar@gmail.com>
2026-07-06 20:25:30 +0530

    Added JUnit Exercises 1, 3 and 4

---
+++ Week-1/TDD using JUnit5 and Mockito/JUnitExercise1/src/test/java/org/example/MainTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void testAddition() {
+        assertEquals(4, 2 + 2);
+    }
+}
