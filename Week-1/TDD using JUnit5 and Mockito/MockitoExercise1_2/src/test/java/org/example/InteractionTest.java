23bee905b6d277cbf03465a33a8ddb70cd1828d3
latika2006 <latikashahapurkar@gmail.com>
2026-07-07 02:41:05 +0530

    Added Mockito and SL4J Exercises

---
+++ Week-1/TDD using JUnit5 and Mockito/MockitoExercise1_2/src/test/java/org/example/InteractionTest.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+public class InteractionTest {
+
+    @Test
+    public void testVerifyInteraction() {
+
+        ExternalApi mockApi = mock(ExternalApi.class);
+
+        MyService service = new MyService(mockApi);
+
+        service.fetchData();
+
+        verify(mockApi).getData();
+    }
+}
