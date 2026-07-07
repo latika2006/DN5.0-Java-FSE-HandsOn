d5ec8790b1f89b23f8557c824fd4d2108b9d947f
latika2006 <latikashahapurkar@gmail.com>
2026-07-07 16:13:36 +0530

    Completed Spring Data JPA HandsOn

---
+++ Week-2/Spring Data JPA with Hibernate/orm-learn/src/main/java/com/cognizant/orm_learn/model/Country.java
@@ -0,0 +1,46 @@
+package com.cognizant.orm_learn.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "country")
+public class Country {
+
+    public Country() {
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Id
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "name")
+    private String name;
+
+}
